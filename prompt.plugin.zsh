482f7c45763f26636ed151de8adaf629ffef89b6
mattmc3 <mattmc3@gmail.com>
2023-12-14 12:07:42 -0500

    Update prompt

--- prompt.plugin.zsh
+++ prompt.plugin.zsh
@@ -18,11 +18,26 @@ fpath=(${0:A:h}/functions $fpath)
 autoload -Uz promptinit && promptinit
 
 # Set the prompt if specified
-local -a prompt_theme
-zstyle -a ':zshzoo:prompt' theme 'prompt_argv'
+typeset -ga prompt_theme
+zstyle -a ':zshzoo:prompt' theme 'prompt_theme' \
+  || prompt_theme=$ZSH_THEME
+
+if [[ ${prompt_theme[1]} == "powerlevel10k" ]]; then
+  # instant prompt
+  typeset -g use_instaprompt
+  if ! zstyle -t ':zshzoo:prompt:powerlevel10k:instaprompt' skip \
+     && [[ ! -v __p9k_instant_prompt_sourced ]]
+  then
+    if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+      source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+    fi
+  fi
+  unset use_instaprompt
+fi
+
 if [[ $TERM == (dumb|linux|*bsd*) ]]; then
   prompt 'off'
-elif (( $#prompt_argv > 0 )); then
-  prompt "$prompt_argv[@]"
+elif (( $#prompt_theme > 0 )); then
+  prompt "$prompt_theme[@]"
 fi
-unset prompt_argv
+unset prompt_theme
